c48e9e43d48935d0493bade006ccbd2bbda52303
NiteshKumar2B <112845749+NiteshKumar2B@users.noreply.github.com>
2023-04-26 09:56:59 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+//
+Welcome Message
+Objective:
+
+To work with the print statement
+
+
+
+Scenario:
+
+Write a program to welcome your friend to your home.
+Get the name of the friend as user input and display the welcome message as given in the sample output.
+
+Sample Input:
+Enter the name: David
+
+Sample Output:
+Hello David! Welcome home.
+//
+friend_name = input("Enter the name:")
+print("Hello " + friend_name + "! Welcome home.")
+
+friend_name = input("Enter the name :")
+print("Hello " + friend_name + "! Welcome home.")
